16040259c8544e7582738f2bcf1e0603545bd242
coucoulesr <coules93@gmail.com>
2019-12-09 20:49:09 -0800

    Add helper state mutators and actions
    Adds setExcercises, addExercise, and removeExerciseById mutators
    Adds populateExercises action and augments login action

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
@@ -22,15 +23,35 @@ export default new Vuex.Store({
       state.token = "";
       localStorage.clear("token");
       state.loggedIn = false;
+    },
+    setExercises(state, exerciseArray) {
+      state.exercises = exerciseArray;
+    },
+    addExercise(state, exerciseObject) {
+      state.exercises.push(exerciseObject);
+    },
+    removeExerciseById(state, id) {
+      state.exercises = state.exercises.filter(ex => ex._id != id);
     }
   },
   actions: {
-    login({ commit }, loginData) {
-      commit("auth", loginData.token);
-      commit("setUser", loginData.username);
+    login(context, loginData) {
+      context.commit("auth", loginData.token);
+      context.commit("setUser", loginData.username);
+      context.dispatch("populateExercises");
     },
     logout({ commit }) {
       commit("logout");
+    },
+    populateExercises({ commit }) {
+      axios
+        .get("http://localhost:5000/exercises/")
+        .then(res => {
+          commit("setExercises", res.data);
+        })
+        .catch(err => {
+          console.error(err);
+        });
     }
   },
   modules: {}
