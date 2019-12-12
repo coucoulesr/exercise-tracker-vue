8e9a76b78b177759bb28066f2f4fc92c0e82c7a4
coucoulesr <coules93@gmail.com>
2019-12-11 16:04:22 -0800

    Allow edit exercise on session restore
    Computes old exercise values and populates them as placeholder in form.
    This adds 1 second delay to page load.

--- src/views/EditExercise.vue
+++ src/views/EditExercise.vue
@@ -1,52 +1,53 @@
 <template>
-  <div>
-    <form class="mt-3" @submit.prevent="editExercise">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-6">
-            <div class="card bg-light">
-              <div class="card-body">
-                <h3 class="font-weight-light mb-3 text-center">Edit Exercise</h3>
-                <div v-if="error" class="col-12 alert alert-danger px-3">{{ error }}</div>
-                <div class="form-row">
-                  <section class="col-sm-12 form-group">
-                    <input
-                      class="form-control"
-                      type="text"
-                      id="description"
-                      placeholder="Description"
-                      name="description"
-                      required
-                      v-model="newDescription"
-                    />
-                  </section>
-                </div>
-                <div class="form-row">
-                  <section class="col-sm-6 form-group">
-                    <input
-                      class="form-control"
-                      type="text"
-                      placeholder="Duration (min)"
-                      required
-                      v-model="newDuration"
-                    />
-                  </section>
-
-                  <section class="col-sm-6 form-group">
-                    <input class="form-control" type="date" required v-model="newDate" />
-                  </section>
-                </div>
-
-                <div class="form-group text-right mb-0">
-                  <input class="btn btn-primary" value="Edit Exercise" type="submit" />
-                  <router-link class="btn btn-secondary ml-2" to="/exercise-log">Cancel</router-link>
-                </div>
-              </div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-6">
+        <div class="card bg-light mt-3">
+          <div class="card-body">
+            <div class="spinner-border text-primary" role="status" v-if="!renderForm">
+              <span class="sr-only">Loading...</span>
             </div>
+            <form @submit.prevent="editExercise" v-if="renderForm">
+              <h3 class="font-weight-light mb-3 text-center">Edit Exercise</h3>
+              <div v-if="error" class="col-12 alert alert-danger px-3">{{ error }}</div>
+              <div class="form-row">
+                <section class="col-sm-12 form-group">
+                  <input
+                    class="form-control"
+                    type="text"
+                    id="description"
+                    placeholder="Description"
+                    name="description"
+                    required
+                    v-model="newDescription"
+                  />
+                </section>
+              </div>
+              <div class="form-row">
+                <section class="col-sm-6 form-group">
+                  <input
+                    class="form-control"
+                    type="text"
+                    placeholder="Duration (min)"
+                    required
+                    v-model="newDuration"
+                  />
+                </section>
+
+                <section class="col-sm-6 form-group">
+                  <input class="form-control" type="date" required v-model="newDate" />
+                </section>
+              </div>
+
+              <div class="form-group text-right mb-0">
+                <input class="btn btn-primary" value="Edit Exercise" type="submit" />
+                <router-link class="btn btn-secondary ml-2" to="/exercise-log">Cancel</router-link>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-    </form>
+    </div>
   </div>
 </template>
 
@@ -55,19 +56,36 @@ export default {
   name: "EditExercise",
   data: function() {
     return {
-      newDate: new Date(),
+      newDate: null,
       newDuration: "",
       newDescription: "",
-      error: ""
+      error: "",
+      renderForm: false
     };
   },
-  mounted: function() {
-    const exercise = this.$store.state.exercises.find(
-      ex => ex._id == this.$route.params.id
-    );
-    this.newDuration = exercise.duration;
-    this.newDescription = exercise.description;
-    this.newDate = exercise.date.substr(0, 10);
+  computed: {
+    exercise() {
+      return this.$store.state.exercises.find(
+        ex => ex._id == this.$route.params.id
+      );
+    },
+    oldDate() {
+      return this.exercise.date.substr(0, 10);
+    },
+    oldDuration() {
+      return this.exercise.duration;
+    },
+    oldDescription() {
+      return this.exercise.description;
+    }
+  },
+  created() {
+    setTimeout(() => {
+      this.newDate = this.oldDate;
+      this.newDuration = this.oldDuration;
+      this.newDescription = this.oldDescription;
+      this.renderForm = true;
+    }, 1000);
   },
   methods: {
     editExercise() {
@@ -79,7 +97,7 @@ export default {
           date: this.newDate
         }
       });
-      setTimeout(() => this.$router.push("/exercise-log"), 1000)
+      setTimeout(() => this.$router.push("/exercise-log"), 1000);
     }
   },
   components: {}
