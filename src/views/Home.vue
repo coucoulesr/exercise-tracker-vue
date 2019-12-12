ea2ac506085ca0719b4c8b8b404a4cc488a309d9
coucoulesr <coules93@gmail.com>
2019-12-12 13:15:23 -0800

    Update Home screen and page title.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,31 +8,41 @@
     <div class="container text-center">
       <div class="row justify-content-center">
         <div class="col-10 col-md-10 col-lg-8 col-xl-7">
-          <h1 class="display-4 text-primary mt-1 mb-2">Meeting Log</h1>
-          <p class="lead mb-4">
-            This simple app creates meetings, allows people to check in, and
-            picks random users to award giveaways. It's a good example of a
-            Single Page Application which includes connection to a database and
-            routing. It's a practical way to learn
-            <a href="https://vuejs.org/">Vue.js</a>
-            with
-            <a href="https://firebase.google.com">Firebase</a>.
+          <h1 class="display-4 text-primary mt-1 mb-2">Exercise Tracker</h1>
+          <p class="lead text-justify mb-4">
+            This single-page application incorporates the MEVN stack (MongoDB,
+            Express.js, Vue.js, and Node.js) to provide a robust user experience
+            based on RESTful principles. This app is a fully-fledged product
+            based on best principles in authorization, security, session and
+            state memory. The source code for the frontend of this application
+            is available on
+            <a href="https://github.com/coucoulesr/exercise-tracker-vue"
+              >GitHub</a
+            >; for more information on this or any of my other projects, visit
+            my website,
+            <a href="https://www.racoucoules.com">racoucoules.com</a> where you
+            can get in contact with me.
           </p>
+          <p class="text-muted mb-4">© 2019 Richard Coucoules</p>
 
           <router-link
             class="btn btn-outline-primary mr-2"
-            to="/register"
-            v-if="!user"
-            >Register</router-link
+            to="/login"
+            v-if="!this.$store.state.loggedIn"
+            >Log In</router-link
           >
           <router-link
             class="btn btn-outline-primary mr-2"
-            to="/login"
-            v-if="!user"
-            >Log In</router-link
+            to="/register"
+            v-if="!this.$store.state.loggedIn"
+            >Register</router-link
           >
-          <router-link class="btn btn-primary" to="/meetings" v-if="user">
-            <font-awesome-icon icon="random" /> Meetings
+          <router-link
+            class="btn btn-primary"
+            to="/exercise-log"
+            v-if="this.$store.state.loggedIn"
+          >
+            <font-awesome-icon icon="random" /> Exercise Log
           </router-link>
         </div>
       </div>
@@ -45,7 +55,7 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 export default {
   name: "home",
-  props: ["user"],
+  props: [],
   components: {
     FontAwesomeIcon
   }
